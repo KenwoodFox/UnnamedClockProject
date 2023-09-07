583fcd6ff85cf4a7c3f94f1d72173a14e3c9fddb
KenwoodFox <kenwood364@gmail.com>
2023-09-07 11:04:46 -0400

    More boilerplate

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -16,12 +16,13 @@ void loop()
 {
   for (uint8_t i = 0; i < 2; i++)
   {
-    digitalWrite(DIR_PIN, i);
-    delay(250);
-    digitalWrite(EN_PIN, true);
-    delay(10 * 1000);
-    digitalWrite(EN_PIN, false);
+    digitalWrite(DIR_PIN, i);    // Set the dir
+    delay(250);                  // Delay while dir is latched
+    digitalWrite(EN_PIN, true);  // Enable movement
+    delay(10 * 1000);            // Wait 10 seconds
+    digitalWrite(EN_PIN, false); // Disable movement
     digitalWrite(DIR_PIN, false);
-    delay(10 * 1000);
+    delay(25 * 1000); // Wait for next minute
+    delay(25 * 1000);
   }
 }
