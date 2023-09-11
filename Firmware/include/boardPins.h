0871309e6c3d3c1247cb74f6eadd6040bb8b34c6
KenwoodFox <kenwood364@gmail.com>
2023-09-11 11:55:49 -0400

    Basics

--- Firmware/include/boardPins.h
+++ Firmware/include/boardPins.h
@@ -4,6 +4,9 @@
 // LEDs
 #define DIR_PIN 3
 #define EN_PIN 2
+
+// GPS
+#define PPS 9 // Change me!
 #define RX_PIN 8
 #define TX_PIN 7
 
--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -3,6 +3,7 @@
  */
 
 #include "Arduino.h"
+#include <Arduino_FreeRTOS.h>
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
 #include <ArduinoLog.h>
@@ -16,8 +17,12 @@ static const uint32_t GPSBaud = 9600;
 SoftwareSerial ss(RX_PIN, TX_PIN);
 TinyGPSPlus gps;
 
-// Headers
+// Task Handlers
+TaskHandle_t TaskBlink_Handler;
+
+// Defs
 void displayInfo();
+void TaskBlink(void *pvParameters);
 
 void setup()
 {
@@ -32,31 +37,63 @@ void setup()
 
   //
   Log.info("Starting version %s", REVISION);
+
+  // Setup tasks
+  xTaskCreate(
+      TaskBlink,           // A pointer to this task in memory
+      "Blink",             // A name just for humans
+      128,                 // This stack size can be checked & adjusted by reading the Stack Highwater
+      NULL,                // Parameters passed to the task function
+      2,                   // Priority, with 2 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+      &TaskBlink_Handler); // Task handle
 }
 
 void loop()
 {
-  /* TODO:
-   * Lots of upgrades have to happen in here, namely:
-   * - Move the CLOCK interface to its own object
-   *  - Need to centralize components like safe power-on-reset (eeprom)
-   *  - Need to ensure that we dont move unless we're 100% sure we can record what we're doing (so we never drift!)
-   *  - RTOS functions like async gps reading, we need to be sure we dont race condition and loose time!
-   */
+  // Empty. Things are done in Tasks.
+}
 
-  digitalWrite(DIR_PIN, gps.time.minute() % 2 == 0); // Set the dir if even/odd
-  delay(250);                                        // Delay while dir is latched
-  digitalWrite(EN_PIN, gps.time.second() < 10);      // Enable movement if first 10 seconds (Change this so that, on the minute mark, the hands have nearly finished moving)
+// void loop()
+// {
+//   /* TODO:
+//    * Lots of upgrades have to happen in here, namely:
+//    * - Move the CLOCK interface to its own object
+//    *  - Need to centralize components like safe power-on-reset (eeprom)
+//    *  - Need to ensure that we dont move unless we're 100% sure we can record what we're doing (so we never drift!)
+//    *  - RTOS functions like async gps reading, we need to be sure we dont race condition and loose time!
+//    */
 
-  while (ss.available() > 0)
-    if (gps.encode(ss.read()))
-      displayInfo();
+//   digitalWrite(DIR_PIN, gps.time.minute() % 2 == 0); // Set the dir if even/odd
+//   delay(250);                                        // Delay while dir is latched
+//   digitalWrite(EN_PIN, gps.time.second() < 10);      // Enable movement if first 10 seconds (Change this so that, on the minute mark, the hands have nearly finished moving)
 
-  if (millis() > 5000 && gps.charsProcessed() < 10)
+//   while (ss.available() > 0)
+//     if (gps.encode(ss.read()))
+//       displayInfo();
+
+//   if (millis() > 5000 && gps.charsProcessed() < 10)
+//   {
+//     Serial.println(F("No GPS detected: check wiring."));
+//     while (true)
+//       ;
+//   }
+// }
+
+void TaskBlink(void *pvParameters) // This is a task.
+{
+  (void)pvParameters;
+
+  // Setup for this task
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // "Loop"
+  for (;;) // A Task shall never return or exit.
   {
-    Serial.println(F("No GPS detected: check wiring."));
-    while (true)
-      ;
+    // Serial.println(11);
+    digitalWrite(LED_BUILTIN, HIGH);       // turn the LED on (HIGH is the voltage level)
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // wait for one second
+    digitalWrite(LED_BUILTIN, LOW);        // turn the LED off by making the voltage LOW
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // wait for one second
   }
 }
 
