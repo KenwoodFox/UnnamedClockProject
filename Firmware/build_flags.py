2d444bcd7bbece58ea549247037bd9ea7549d9af
KenwoodFox <kenwood364@gmail.com>
2023-10-22 22:45:07 -0400

    Make build_flags.py portable

--- Firmware/build_flags.py
+++ Firmware/build_flags.py
@@ -5,42 +5,51 @@ from datetime import date
 
 today = date.today()
 
-revision = (
-    subprocess.check_output(
-        [
-            "git",
-            "describe",
-            "--abbrev=8",
-            "--dirty",
-            "--always",
-            "--tags",
-        ]
+try:
+    revision = (
+        subprocess.check_output(
+            [
+                "git",
+                "describe",
+                "--abbrev=8",
+                "--dirty",
+                "--always",
+                "--tags",
+            ]
+        )
+        .strip()
+        .decode("utf-8")
     )
-    .strip()
-    .decode("utf-8")
-)
+except:
+    revision = "Unknown"
 
-host = (
-    subprocess.check_output(
-        [
-            "hostname",
-        ]
+try:
+    host = (
+        subprocess.check_output(
+            [
+                "hostname",
+            ]
+        )
+        .strip()
+        .decode("utf-8")
     )
-    .strip()
-    .decode("utf-8")
-)
+except:
+    host = "Unknown"
 
-username = (
-    subprocess.check_output(
-        [
-            "id",
-            "-u",
-            "-n",
-        ]
+try:
+    username = (
+        subprocess.check_output(
+            [
+                "id",
+                "-u",
+                "-n",
+            ]
+        )
+        .strip()
+        .decode("utf-8")
     )
-    .strip()
-    .decode("utf-8")
-)
+except:
+    username = "Unknown"
 
 # Cleanup CI
 if username == "root":
