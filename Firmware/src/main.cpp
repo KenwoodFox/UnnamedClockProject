406acd3dce983ff8fc3ecf3cde888583a43a8c89
KenwoodFox <kenwood364@gmail.com>
2023-10-26 13:57:37 -0400

    Slim

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -16,6 +16,23 @@ static const int pauseTime = 2 * 1000; // Time between moves
 // Objects
 TinyGPSPlus gps;
 
+// Heap/global memory
+bool timeIsSet = false;   // True when the user has set the time
+int controllerHour = 0;   // The hour the controller thinks it is (0-11)
+int controllerMinute = 0; // The minute the controller thinks it is (0-59)
+
+// States
+enum State
+{
+  Paused,
+  MoveL1,
+  StopL1,
+  MoveL2,
+  StopL2
+};
+
+State currentState = Paused; // Global state
+
 void setup()
 {
   // Begin
@@ -24,15 +41,52 @@ void setup()
 
   Log.infoln(F("Starting version %s"), REVISION);
 
+  // Configure hardware
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // Configure software
+  Serial.setTimeout(5000);
 
   Log.infoln(F("Done."));
 }
 
 void loop()
 {
+  switch (currentState)
+  {
+  case Paused:
+    /* what to do when paused */
+    break;
+
+  default:
+    // How did we get here?..
+    currentState = Paused;
+    break;
+  }
+
   delay(1000);
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
+
+  Log.infoln(F("Hour is %d, minute is %d"), controllerHour, controllerMinute);
+}
+
+// Super simple serial event (called from loop)
+void serialEvent()
+{
+  Log.warningln(F("Please enter hour [0-11]:"));
+  int _hourToSet = Serial.parseInt();
+
+  Log.warningln(F("Please enter minute [0-59]:"));
+  int _minToSet = Serial.parseInt();
+
+  // clear buf
+  while (Serial.available())
+  {
+    Serial.read();
+  }
+
+  controllerHour = _hourToSet;
+  controllerMinute = _minToSet;
 }
